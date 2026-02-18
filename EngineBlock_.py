90ea278988e969506f82e10e7f14a466ab73c51b
z003x1np <z003x1np@DISW6ZW4CY3>
2026-02-18 16:40:44 +0530

    Upload

--- EngineBlock_.py
+++
@@ -1,4 +0,0 @@
-ApplyUpdateAssistant(SC('EngineBlock_eeb82acf4f1d4d81aaad7c43303aab0e'))
-#Command logging not implemented for command SIMClearSelectionCmd
-AMERemoveComponent("m6dofassembly", None)
-AMEAddComponent("tr.vehicle_viewer", "vehicle_viewer", (498,128), True, SC(''))
